9c3c2148a2fb0993ab3d24c083357e8b17443fae
Eamon Burns <eamon.r.burns@gmail.com>
2024-01-31 00:48:30 -0800

    Start following sql db tutorial
    Add docker compose file and connection to database

---
+++ sql/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+    _ "github.com/lib/pq"
+)
+
+func main() {
+    connStr := "postgres://postgres:secret@localhost:5432/gopgtest?sslmode=disable"
+    
+    db, err := sql.Open("postgres", connStr)
+    defer db.Close()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    if err = db.Ping(); err != nil {
+        log.Fatal(err)
+    }
+
+    log.Print("Successfully connected to database")
+}
