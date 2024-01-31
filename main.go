5692d5a33b576b803105e1a8d2662a9e6d6e2e56
Eamon Burns (BTC WSL) <eamon@eamons-drive.beltec.us>
2024-01-30 17:29:07 -0800

    Use go template instead of raw text

--- main.go
+++ main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+type Film struct {
+    Title string
+    Director string
+}
 
 func main() {
     fmt.Println("Running htmx server...")
 
     h1 := func (w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello world!\n")
-        io.WriteString(w, r.Method)
+
+        films := map[string][]Film{
+            "Films": {
+                { Title: "The Godfather", Director: "Francis Ford Coppola" },
+                { Title: "Blade Runner", Director: "Ridley Scott" },
+                { Title: "The Thing", Director: "John Carpenter" },
+            },
+        }
+
+        tmpl := template.Must(template.ParseFiles("index.html"))
+        tmpl.Execute(w, films)
     }
 
     http.HandleFunc("/", h1)
