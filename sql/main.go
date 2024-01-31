f508cafb1c1ddc3ba8ed228a5f6f010ca1a2c9d3
Eamon Burns (BTC WSL) <eamon@eamons-drive.beltec.us>
2024-01-31 15:28:38 -0800

    Add function to insert product

--- sql/main.go
+++ sql/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
+
+type Product struct {
+    Name string
+    Price float64
+    Available bool
+}
 
 func main() {
     connStr := "postgres://postgres:secret@localhost:5432/gopgtest?sslmode=disable"
@@ -24,6 +31,15 @@ func main() {
     log.Print("Connected to database...")
 
     createProductTable(db)
+
+    product := Product{
+        Name: "Book",
+        Price: 15.55,
+        Available: true,
+    }
+    pk := insertProduct(db, product)
+
+    fmt.Printf("ID = %d\n", pk)
 }
 
 func createProductTable(db *sql.DB) {
@@ -39,4 +55,18 @@ func createProductTable(db *sql.DB) {
     if err != nil {
         log.Fatal(err)
     }
+}
+
+func insertProduct(db *sql.DB, product Product) int {
+    query := `INSERT INTO product (name, price, available)
+        VALUES ($1, $2, $3) RETURNING id`
+
+    var pk int
+
+    err := db.QueryRow(query, product.Name, product.Price, product.Available).Scan(&pk)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return pk
 }
