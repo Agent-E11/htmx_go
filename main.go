126a5d5d4a73a883db07f41070e6d7ee18073bda
Eamon (WSL Laptop) <eamon.r.burns@gmail.com>
2024-02-02 00:01:05 -0800

    Extract connection to database out to function

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/agent-e11/htmx_go/handlers"
 	mySql "github.com/agent-e11/htmx_go/sql"
@@ -18,20 +15,7 @@ type Film struct {
 }
 
 func main() {
-    password := os.Getenv("POSTGRES_PASSWORD")
-    port := os.Getenv("POSTGRES_PORT")
-    dbName := os.Getenv("POSTGRES_DBNAME")
-    if password == "" { log.Fatal("Error reading POSTGRES_PASSWORD") }
-    if port != "5432" && port != "" {
-        log.Fatal("Different port numbers are unsupported at this time. Use port 5432")
-    } else {
-        port = "5432"
-    }
-    if dbName == "" { log.Fatal("Error reading POSTGRES_DBNAME") }
-
-    connStr := fmt.Sprintf("postgres://postgres:%s@localhost:%s/%s?sslmode=disable", password, port, dbName)
-    
-    db, err := sql.Open("postgres", connStr)
+    db, err := tools.ConnectDatabase()
     defer db.Close()
 
     if err != nil {
@@ -45,13 +29,15 @@ func main() {
     log.Print("Connected to database...")
 
     mySql.CreateProductTable(db)
-    err = tools.LoadDummyData(db, "dummy.json")
-    if err != nil {
-        log.Fatalf("Error loading dummy data: %v\n", err)
-    }
+
+    // Load dummy data
+    //err = tools.LoadDummyData(db, "dummy.json")
+    //if err != nil {
+    //    log.Fatalf("Error loading dummy data: %v\n", err)
+    //}
 
     // HTTP Server
-    fmt.Println("Running htmx server...")
+    log.Print("Running htmx server...")
 
     http.HandleFunc("/", handlers.HomePage)
     http.HandleFunc("/add-product/", handlers.AddProduct)
