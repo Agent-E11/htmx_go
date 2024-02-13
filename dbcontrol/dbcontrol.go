d325193516ce321812ec101d975c41a40de22329
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-02-12 20:50:18 -0800

    Remove unused main function from dbcontrol

--- dbcontrol/dbcontrol.go
+++ dbcontrol/dbcontrol.go
@@ -1,14 +1,14 @@
 package dbcontrol
 
 import (
-	"database/sql"
+    "database/sql"
     "errors"
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 
-	_ "github.com/joho/godotenv/autoload"
-	_ "github.com/lib/pq"
+    _ "github.com/joho/godotenv/autoload"
+    _ "github.com/lib/pq"
 )
 
 type Product struct {
@@ -54,65 +54,6 @@ func ConnectDatabase() (*sql.DB, error) {
     
     // Return the database connection and error
     return sql.Open("postgres", connStr)
-}
-
-// FIXME: Remove this entire function
-func main() {
-    password := os.Getenv("POSTGRES_PASSWORD")
-    port := os.Getenv("POSTGRES_PORT")
-    dbName := os.Getenv("POSTGRES_DBNAME")
-    if password == "" { log.Fatal("Error reading POSTGRES_PASSWORD") }
-    if port != "5432" && port != "" {
-        log.Fatal("Different port numbers are unsupported at this time. Use port 5432")
-    } else {
-        port = "5432"
-    }
-    if dbName == "" { log.Fatal("Error reading POSTGRES_DBNAME") }
-
-    connStr := fmt.Sprintf("postgres://postgres:%s@localhost:%s/%s?sslmode=disable", password, port, dbName)
-    
-    db, err := sql.Open("postgres", connStr)
-    defer db.Close()
-
-    if err != nil {
-        log.Fatalf("Error opening db: %v\n", err)
-    }
-
-    if err = db.Ping(); err != nil {
-        log.Fatalf("Error pinging db: %v\n", err)
-    }
-
-    log.Print("Connected to database...")
-
-    CreateProductTable(db)
-
-    data := []Product{}
-    rows, err := db.Query("SELECT name, price, available FROM product")
-    if err != nil {
-        log.Fatalf("Error querying db: %v\n", err)
-    }
-    defer rows.Close()
-
-    var name string
-    var price float64
-    var available bool
-
-    for rows.Next() {
-        err := rows.Scan(&name, &price, &available)
-        if err != nil {
-            log.Fatal(err)
-        }
-        
-        data = append(data, Product{ name, price, available })
-    }
-
-    fmt.Println(data)
-
-    p := Product{ "Book", 15.55, true }
-
-    pk := InsertProduct(db, p)
-
-    fmt.Println("ID =", pk)
 }
 
 func CreateProductTable(db *sql.DB) {
